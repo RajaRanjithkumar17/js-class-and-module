f6751e4a7b90f52762141d5b275a741ac5a8e5c7
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-10-14 10:00:57 +0530

    my commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import { details } from "./script";
+console.log(details.name);
+import { personal } from "./script";
+import { a } from "./script";
+
+import * as rr from "./script";
+
+console.log(rr.details);
---
+++ script.js
@@ -0,0 +1,25 @@
+export class  details {
+    constructor(){
+        this.name="ranjith"
+        this.email = "rajaranjithkumar17@gmail.com"
+    }
+}
+console.log(typeof details,details);
+ export var a =100
+
+
+export  class  personal {
+    constructor(){
+        this.name="ranjith"
+        this.email = "rajaranjithkumar17@gmail.com"
+        this.age = 16
+    }
+}
+
+export class  personal {
+    constructor(){
+        this.name="ranjith"
+        this.email = "rajaranjithkumar17@gmail.com"
+        this.age = 16
+    }
+}
